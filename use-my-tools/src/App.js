1014de37276f41deacadcfe7e7bf8c7e064399d6
alec blakeley <alecblkly@gmail.com>
2019-09-23 16:17:35 -0400

    Built out structure for navigation, created js file for base page after login

--- use-my-tools/src/App.js
+++ use-my-tools/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      {/* Routes to Nav items below */}
     </div>
   );
 }
---
+++ use-my-tools/src/components/Navigation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    width: 12rem;
+    background-color: #ecfffd;
+    border-radius: 10px;
+    height: 5vh;
+    margin: 1%
+`;
+
+const TopBar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;  
+    width: 100%;
+    height: 10vh;
+    background-color: #76d275;
+    border: 2px solid #88c399
+`;
+
+const Title = styled.h1`
+    display: flex;
+    align-items: center;
+    justify-content: center
+`;
+
+const ButtonDiv = styled.div`
+    display: flex;
+`;
+
+const Navigation = () => {
+    return (
+        // Navigation Links
+        // Navigation Links will be styled as buttons
+        // Login/Register buttons change to User Tools / Marketplace once logged in
+        // Drop down user name - logout/account settings
+        <TopBar className="top-bar">
+            <Title>Use My Tools</Title>
+            <ButtonDiv>
+                <Button><NavLink to="/login">Login</NavLink></Button>
+                <Button><NavLink to="/register">Register</NavLink></Button>
+                <Button><NavLink to="/Marketplace">Marketplace</NavLink></Button>
+                <Button><NavLink to="/My-Tools">My Tools</NavLink></Button>
+            </ButtonDiv>
+        </TopBar>
+    );
+}
+
+export default Navigation;
