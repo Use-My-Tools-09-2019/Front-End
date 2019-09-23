4ea2cf7b488414ebb3805f77a15f9b53f1f01402
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-23 15:20:01 -0500

    added login and register components

--- use-my-tools/src/App.js
+++ use-my-tools/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+
+//components
+import Register from './components/Register'
+
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Register/>
     </div>
   );
 }
---
+++ use-my-tools/src/components/Register.jsx
@@ -0,0 +1,167 @@
+import React from 'react'
+import { withFormik, Form, Field } from "formik";
+
+import * as Yup from 'yup'
+
+//styles
+import { Button } from 'semantic-ui-react'
+function Register({ touched, errors, status }) {
+    return (
+        <>
+        <h1>Register</h1>
+        <Form className="form">
+            <div className='ui input'>
+                <Field
+                    placeholder="Enter your email"
+                    name="email"
+                    type="email"
+                />
+            </div>
+            <p>{touched.email && errors.email}</p>
+            <div className='ui input'>
+                <Field
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                />
+           </div>
+					 <h2>Address</h2>
+           <p>{touched.street && errors.street}</p>
+					 <div className='ui input'>
+                <Field
+                    placeholder="street"
+                    name="street"
+                    type="text"
+                />
+           </div>
+					 <p>{touched.apartment && errors.apartment}</p>
+					 <div className='ui input'>
+                <Field
+                    placeholder="apartment"
+                    name="apartment"
+                    type="text"
+                />
+           </div>
+           <p>{touched.city && errors.city}</p>
+					 <div className='ui input'>
+                <Field
+                    placeholder="city"
+                    name="city"
+                    type="text"
+                />
+           </div>
+					 <p>{touched.state && errors.state}</p>
+					 <div >
+						 		<label>State:  </label>
+                <Field
+                    placeholder="state"
+                    name="state"
+										component="select"
+								>
+										<option value="AL">Alabama</option>
+										<option value="AK">Alaska</option>
+										<option value="AZ">Arizona</option>
+										<option value="AR">Arkansas</option>
+										<option value="CA">California</option>
+										<option value="CO">Colorado</option>
+										<option value="CT">Connecticut</option>
+										<option value="DE">Delaware</option>
+										<option value="DC">District Of Columbia</option>
+										<option value="FL">Florida</option>
+										<option value="GA">Georgia</option>
+										<option value="HI">Hawaii</option>
+										<option value="ID">Idaho</option>
+										<option value="IL">Illinois</option>
+										<option value="IN">Indiana</option>
+										<option value="IA">Iowa</option>
+										<option value="KS">Kansas</option>
+										<option value="KY">Kentucky</option>
+										<option value="LA">Louisiana</option>
+										<option value="ME">Maine</option>
+										<option value="MD">Maryland</option>
+										<option value="MA">Massachusetts</option>
+										<option value="MI">Michigan</option>
+										<option value="MN">Minnesota</option>
+										<option value="MS">Mississippi</option>
+										<option value="MO">Missouri</option>
+										<option value="MT">Montana</option>
+										<option value="NE">Nebraska</option>
+										<option value="NV">Nevada</option>
+										<option value="NH">New Hampshire</option>
+										<option value="NJ">New Jersey</option>
+										<option value="NM">New Mexico</option>
+										<option value="NY">New York</option>
+										<option value="NC">North Carolina</option>
+										<option value="ND">North Dakota</option>
+										<option value="OH">Ohio</option>
+										<option value="OK">Oklahoma</option>
+										<option value="OR">Oregon</option>
+										<option value="PA">Pennsylvania</option>
+										<option value="RI">Rhode Island</option>
+										<option value="SC">South Carolina</option>
+										<option value="SD">South Dakota</option>
+										<option value="TN">Tennessee</option>
+										<option value="TX">Texas</option>
+										<option value="UT">Utah</option>
+										<option value="VT">Vermont</option>
+										<option value="VA">Virginia</option>
+										<option value="WA">Washington</option>
+										<option value="WV">West Virginia</option>
+										<option value="WI">Wisconsin</option>
+										<option value="WY">Wyoming</option>
+									</Field>
+           </div>
+					 <p>{touched.zipcode && errors.zipcode}</p>
+					 <div className='ui input'>
+                <Field
+                    placeholder="zipcode"
+                    name="zipcode"
+                    type="text"
+                />
+           </div>
+           <Button color="blue">Register</Button>
+           <br />
+           {status && <h3 style={{color: 'red'}}>Please try again, error during signup</h3>}
+      </Form>
+      </>
+    )
+}
+
+ const FormikRegister = withFormik({
+    mapPropsToValues({ email, password, street, apartment, city, state, zipcode}) {
+      return {
+        email: email || "",
+				password: password || "",
+				street: street || "",
+				apartment: apartment || "",
+				city: city || "",
+				state: state || "",
+				zipcode: zipcode || ""
+
+      };
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required("Username is required"),
+			password: Yup.string().min(8).required("Password is ALSO required"),
+      street: Yup.string().required("Street is required"),
+      apartment: Yup.string(),
+			city: Yup.string().required("City is required"),
+			state: Yup.string(),
+      zipcode: Yup.string().required("Zipcode is required"),
+    }),
+
+    handleSubmit(values, { setStatus }) {
+
+      // axios
+      //   .post(url, propsToSubmit)
+      //   .then(results => {
+      //     props.props.history.push('/login')
+      //   })
+      //   .catch(error => {
+      //     console.log(error)
+      //     props.setStatus(error.response.data.message)
+      //   })
+    }
+	})(Register);
+	
+	export default FormikRegister
