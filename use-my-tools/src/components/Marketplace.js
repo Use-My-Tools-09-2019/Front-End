1bc9cd96ee1c6cfefa491c4cbbdea8cfbb503a30
David Kuseh <davegru203@gmail.com>
2019-09-24 19:44:26 +0000

    added request tool form, not styled

--- use-my-tools/src/components/Marketplace.js
+++ use-my-tools/src/components/Marketplace.js
@@ -3,7 +3,6 @@ import  DropdownTools  from './SearchForm';
 import ToolCard from './ToolCard';
 import styled from 'styled-components';
 
-
 const ToolsPage = styled.div`
     width: 90%;
     display: flex;
@@ -48,53 +47,53 @@ const toolsList = [
       "price": "$15/day"
     },
     {
-        "name": "Angle-grinder",
-        "picture": "http://placehold.it/32x32",
-        "about": "Hand-held angle-grinder powered by electric motor",
-        "price": "$7/day"
+      "name": "Angle-grinder",
+      "picture": "http://placehold.it/32x32",
+      "about": "Hand-held angle-grinder powered by electric motor",
+      "price": "$7/day"
       },
-      {
-        "name": "Jackhammer",
-        "picture": "http://placehold.it/32x32",
-        "about": "Hand-held jackhammer powered by electric motor",
-        "price": "$18/day"
+    {
+      "name": "Jackhammer",
+      "picture": "http://placehold.it/32x32",
+      "about": "Hand-held jackhammer powered by electric motor",
+      "price": "$18/day"
       },
-      {
-        "name": "Power-wrench",
-        "picture": "http://placehold.it/32x32",
-        "about": "Hand-held power-wrench that uses compressed air",
-        "price": "$8/day"
+    {
+      "name": "Power-wrench",
+      "picture": "http://placehold.it/32x32",
+      "about": "Hand-held power-wrench that uses compressed air",
+      "price": "$8/day"
       },
-      {
-        "name": "Chainsaw",
-        "picture": "http://placehold.it/32x32",
-        "about": "Electric motor powered chainsaw",
-        "price": "$15/day"
+    {
+      "name": "Chainsaw",
+      "picture": "http://placehold.it/32x32",
+      "about": "Electric motor powered chainsaw",
+      "price": "$15/day"
       },
-      {
-        "name": "Power-wrench",
-        "picture": "http://placehold.it/32x32",
-        "about": "Hand-held power-wrench that uses compressed air",
-        "price": "$8/day"
+    {
+      "name": "Power-wrench",
+      "picture": "http://placehold.it/32x32",
+      "about": "Hand-held power-wrench that uses compressed air",
+      "price": "$8/day"
       },
-      {
-        "name": "Chainsaw",
-        "picture": "http://placehold.it/32x32",
-        "about": "Electric motor powered chainsaw",
-        "price": "$15/day"
+    {
+      "name": "Chainsaw",
+      "picture": "http://placehold.it/32x32",
+      "about": "Electric motor powered chainsaw",
+      "price": "$15/day"
       },
-      {
-        "name": "Power-wrench",
-        "picture": "http://placehold.it/32x32",
-        "about": "Hand-held power-wrench that uses compressed air",
-        "price": "$8/day"
+    {
+      "name": "Power-wrench",
+      "picture": "http://placehold.it/32x32",
+      "about": "Hand-held power-wrench that uses compressed air",
+      "price": "$8/day"
       },
-      {
-        "name": "Chainsaw",
-        "picture": "http://placehold.it/32x32",
-        "about": "Electric motor powered chainsaw",
-        "price": "$15/day"
-      }
+    {
+      "name": "Chainsaw",
+      "picture": "http://placehold.it/32x32",
+      "about": "Electric motor powered chainsaw",
+      "price": "$15/day"
+    }
   ];
 
     return (
