61146910a8fa1bdc9159bfb4a44475fb30965f1b
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-05-05 17:37:46 -0500

    removed console log from useInput

--- use-my-tools/src/utils/hooks/useInput.js
+++ use-my-tools/src/utils/hooks/useInput.js
@@ -3,7 +3,6 @@ import {useState} from 'react'
 export const useInput = initialValue => {
     const [value, setValue] = useState(initialValue);
     const handleChanges = e => {
-      console.log('e', e)
       setValue({...value, [e.target.name]: e.target.value});
     };
     return [value, setValue, handleChanges];
