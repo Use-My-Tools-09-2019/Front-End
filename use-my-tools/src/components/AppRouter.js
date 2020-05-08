700d62300da630cc2355dfac7d52a521fedf3aa8
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-05-08 16:03:41 -0500

    styling fixes

--- use-my-tools/src/components/AppRouter.js
+++ use-my-tools/src/components/AppRouter.js
@@ -1,57 +1,92 @@
-import React from 'react'
+import React from "react";
 
 //router
-import { BrowserRouter as Router, Route, Redirect, Switch, NavLink } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+  NavLink,
+} from "react-router-dom";
 
 //components
-import Dashboard from './Dashboard';
-import Login from './Login';
-import Register from './register/Register';
-import Marketplace from './Marketplace';
-import PrivateRoute from './PrivateRoute';
-import MyTools from './MyTools';
-import Footer from './Footer';
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Register from "./register/Register";
+import Marketplace from "./Marketplace";
+import PrivateRoute from "./PrivateRoute";
+import MyTools from "./MyTools";
+import Footer from "./Footer";
 
 //styles
-import * as styled from './styled-components/navStyles'
+import * as styled from "./styled-components/navStyles";
+import logImg from "../assets/logo.png";
+
 
 export default function AppRouter() {
-    return (
-        <Router>
-            <styled.TopBar class="top-bar">
-                <styled.Title>Use My Tools</styled.Title>
-                <styled.ButtonDiv>
-                    {!localStorage.getItem('token') 
-                    ?
-                    <>
-                        <styled.Button><NavLink to="/login">Login</NavLink></styled.Button>
-                        <styled.Button><NavLink to="/register">Register</NavLink></styled.Button>
-                    </>
-                    :
-                    <>
-                        <styled.Button><NavLink to={`/marketplace/${localStorage.getItem('username')}`}>Marketplace</NavLink></styled.Button>
-                        <styled.Button><NavLink to={`/my-tools/${localStorage.getItem('username')}`}>My Tools</NavLink></styled.Button>
-                        <span>
-                            <styled.Button
-                            onClick ={() => {
-                                localStorage.removeItem("token");
-                                localStorage.removeItem('username');
-                                window.location.reload()
-                            }}
-                            ><NavLink to="/login" >Logout</NavLink></styled.Button>
-                        </span>
-                    </>
-                    }
-            
-                </styled.ButtonDiv>
-            </styled.TopBar>
-            <Switch>
-                <Route exact path = "/" render={() => localStorage.getItem('token') ? <Redirect to={`/dashboard/${localStorage.getItem('username')}`}/> : <Redirect to={`/login`} />} />
-                <Route exact path='/login' component={Login}/>
-                <Route exact path='/register' component={Register}/>
-                <PrivateRoute exact path='/marketplace/:id' component={Marketplace}/>
-                <PrivateRoute path="/my-Tools/:id" component={MyTools} />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <styled.TopBar class="top-bar">
+        <styled.ButtonDiv>
+          <styled.Logo src={logImg} />
+          <styled.Title>Use My Tools</styled.Title>
+        </styled.ButtonDiv>
+        <styled.ButtonDiv>
+          {!localStorage.getItem("token") ? (
+            <>
+              <styled.Button>
+                <NavLink to="/login">Login</NavLink>
+              </styled.Button>
+              <styled.Button>
+                <NavLink to="/register">Register</NavLink>
+              </styled.Button>
+            </>
+          ) : (
+            <>
+              <styled.Button>
+                <NavLink
+                  to={`/marketplace/${localStorage.getItem("username")}`}
+                >
+                  Marketplace
+                </NavLink>
+              </styled.Button>
+              <styled.Button>
+                <NavLink to={`/my-tools/${localStorage.getItem("username")}`}>
+                  My Tools
+                </NavLink>
+              </styled.Button>
+              <span>
+                <styled.Button
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("username");
+                    window.location.reload();
+                  }}
+                >
+                  <NavLink to="/login">Logout</NavLink>
+                </styled.Button>
+              </span>
+            </>
+          )}
+        </styled.ButtonDiv>
+      </styled.TopBar>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() =>
+            localStorage.getItem("token") ? (
+              <Redirect to={`/dashboard/${localStorage.getItem("username")}`} />
+            ) : (
+              <Redirect to={`/login`} />
+            )
+          }
+        />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <PrivateRoute exact path="/marketplace/:id" component={Marketplace} />
+        <PrivateRoute path="/my-Tools/:id" component={MyTools} />
+      </Switch>
+    </Router>
+  );
 }
--- use-my-tools/src/components/styled-components/navStyles.js
+++ use-my-tools/src/components/styled-components/navStyles.js
@@ -1,47 +1,54 @@
 import styled from 'styled-components';
 
+
 export {
-    //nav
     Button,
     TopBar,
     Title,
-    ButtonDiv
+    ButtonDiv,
+    Logo
 }
 
 
-const Button = styled.button`
-    width: 12rem;
-    background-color: #ecfffd;
-    border-radius: 10px;
-    height: 5rem;
-    margin: 1%;
-    font-size: 1.6rem;
-    box-shadow: 4px 4px 4px 0px #000
-`;
 
 const TopBar = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;  
-    width: 100%;
-    height: 10rem;
-    background-color:#269;
-    background-image: linear-gradient(white 2px, transparent 2px),
-    linear-gradient(90deg, white 2px, transparent 2px),
-    linear-gradient(rgba(255,255,255,.3) 1px, transparent 1px),
-    linear-gradient(90deg, rgba(255,255,255,.3) 1px, transparent 1px);
-    background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
-    background-position:-2px -2px, -2px -2px, -1px -1px, -1px -1px;
+display: flex;
+justify-content: space-between;
+align-items: center; 
+padding: 0 20px
+width: 100%;
+height: 100px;
+color: yellow;
+background: black
 
 `;
 
 const Title = styled.h1`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 2.5rem
+font-size: 2.5rem
+margin-left: 10px
 `;
 
+const Logo = styled.img`
+height: 80px;
+object-fit: contain;
+`
+
 const ButtonDiv = styled.div`
-    display: flex;
+display: flex;
+`;
+
+const Button = styled.div`
+width: 10rem;
+height: 3rem;
+margin: 1%;
+font-size: 2rem;
+color: yellow;
+background: black;
+border: none;
+padding: 1rem 2rem;
+margin: 0;
+text-decoration: none;
+font-family: sans-serif;
+font-size: 1rem;
+cursor: pointer;
 `;
