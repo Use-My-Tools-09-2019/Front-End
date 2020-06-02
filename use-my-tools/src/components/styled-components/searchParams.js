ae8875de3000aa09fab225c38d0e843c8bfad0c0
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-06-02 12:59:35 -0500

    created searchParams styled components file

---
+++ use-my-tools/src/components/styled-components/searchParams.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export{
+    Container,
+    Button
+}
+
+const Container = styled.div`
+    display: flex;
+    margin-left: 20px;
+    
+    
+`
+
+const Button = styled.div`
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 10px;
+    background: ${props => props.active ? 'yellow': 'none'};
+    border 1px solid yellow;
+    color: ${props => props.active ? 'black': 'yellow'};
+    width: 150px;
+    height: 50px;
+    &:hover {
+        background: ${props => props.active ? '': '#252525'};
+    }
+`
