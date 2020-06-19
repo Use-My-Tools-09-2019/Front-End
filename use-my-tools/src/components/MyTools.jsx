fac386f7ed23c0f8689edb2ba9c4d4ca2d9565fe
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-06-19 15:46:35 -0500

    added image uploader and refactured myTools

--- use-my-tools/src/components/MyTools.jsx
+++ use-my-tools/src/components/MyTools.jsx
@@ -1,54 +1,37 @@
 import React, { useState, useEffect } from "react";
 
-import welding from "../images/welding.jpg";
-
 //styles
-import { FaWindowClose } from "react-icons/fa";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import * as styled from "./styled-components/myTools"
-import * as color from '../styles/color'
+import * as styled from "./styled-components/myTools";
+import * as cardStyles from "./styled-components/toolCard";
+
+import * as color from "../styles/color";
 
 //redux
 import { useDispatch, useSelector } from "react-redux";
-import { deleteTool, getUserTools } from "../store/actions";
+import { getUserTools } from "../store/actions";
 
 // components
 import AddTool from "./AddTool";
 import Loader from "react-loader-spinner";
-// import UpdateToolModal from "./UpdateToolModal"
-
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+import ImageUploader from "react-images-upload";
+import UpdateToolModal from "./UpdateToolModal";
+import DeleteTool from "./DeleteTool";
 
 const MyTools = () => {
   //redux hooks
-  const dispatch = useDispatch();
   const userTools = useSelector((state) => state.tools.userTools);
   const status = useSelector((state) => state.tools.status);
+  const dispatch = useDispatch();
+
+  //add tool modal
+  const [modal, setModal] = useState(false);
+  const handleModalOpen = () => {
+    setModal(true);
+  };
+
+  const handleModalClose = () => {
+    setModal(false);
+  };
 
 
   //side effects
@@ -56,64 +39,59 @@ const MyTools = () => {
     dispatch(getUserTools());
   }, []);
 
-  //mui modal
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const [modalStyle] = useState(getModalStyle);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  if (status){
-    return(
+  //spinner logic
+  if (status) {
+    return (
       <div>
-        <br/>
-        <Loader
-          type="Oval"
-          color={color.spinner}
-          height={100}
-          width={100}
-        />
+        <br />
+        <Loader type="Oval" color={color.spinner} height={100} width={100} />
       </div>
-    )
+    );
   }
 
   return (
     <>
-      <br/>
-      <AddTool />
+      <br />
+      <AddTool
+        handleModalClose={handleModalClose}
+        handleModalOpen={handleModalOpen}
+        modal={modal}
+      />
       <styled.ContainerDiv>
-          {/* Mapping over tools for the user, adding new card for each input */}
-          {userTools.map((tool) => (
-                  <div>
+        {/* Mapping over tools for the user, adding new card for each input */}
+        {userTools.map((tool) => (
+          <cardStyles.Card>
+            <cardStyles.ToolContent>
 
-                    <header icon="delete" content="Delete Tool" 
-                      onClick={handleOpen}
-                    />
-                    <Modal
-                      open={open}
-                      onClose={handleClose}
-                      aria-labelledby="simple-modal-title"
-                      aria-describedby="simple-modal-description"
-                    >
-                      <div style={modalStyle} className={classes.paper}>
-                        <p>Are you sure you want to delete this tool?</p>
-                        <button
-                          onClick={() => {
-                            dispatch(deleteTool(tool.id));
-                          }}
-                        >
-                          Yes
-                        </button>
-                      </div>
-                    </Modal>
-                  </div>
-          ))}
+              <styled.ButtonDiv>
+                <DeleteTool tool={tool} />
+                <UpdateToolModal tool={tool} />
+              </styled.ButtonDiv>
+
+              <cardStyles.ToolTitle>{tool.tool_name}</cardStyles.ToolTitle>
+              {tool.img_url === null ? (
+                <>
+                  <ImageUploader
+                    withIcon={true}
+                    buttonText="Choose image"
+                    onChange={null}
+                    imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                    maxFileSize={5242880}
+                    fileContainerStyle={{
+                      background: color.cardBackground,
+                    }}
+                  />
+                  <styled.ImgHr />
+                </>
+              ) : (
+                <img src={tool.img_url} />
+              )}
+              <h4>Type: {tool.tool_type}</h4>
+              <h4>Tool Description: {tool.tool_description}</h4>
+              <h4>Tool Price: ${tool.rental_cost}</h4>
+            </cardStyles.ToolContent>
+          </cardStyles.Card>
+        ))}
       </styled.ContainerDiv>
     </>
   );
