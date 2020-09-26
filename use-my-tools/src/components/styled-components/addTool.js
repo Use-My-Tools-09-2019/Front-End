286efdd4e22ed00edfdac9da04fe86b1cc5c2e27
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-09-25 22:34:56 -0500

    more style for addTool modal

--- use-my-tools/src/components/styled-components/addTool.js
+++ use-my-tools/src/components/styled-components/addTool.js
@@ -3,5 +3,11 @@ import * as color from '../../styles/color'
 import { tablet } from './media'
 
 export {
-    
+    Title
 }
+
+const Title = styled.h1`
+    color: ${color.primary};
+    font-size: 2.2rem;
+    padding-bottom: 25px;
+`
