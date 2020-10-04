75dfe46d85fc8f6d696f77d69e50e35482eca7fe
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-10-04 17:23:14 -0500

    added logging in status for spinner

--- use-my-tools/src/store/actions/loginActions.js
+++ use-my-tools/src/store/actions/loginActions.js
@@ -1,12 +1,26 @@
 
+
+
+export const LOGIN_START = 'LOGIN_START'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
+
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
 
 
 
 
-export const loginAC = (credentials) => dispatch => {
+export const loginStart = () => dispatch => {
+    dispatch({ type: LOGIN_START, })
+}
+
+export const loginSuccess = (credentials) => dispatch => {
     dispatch({ type: LOGIN_SUCCESS, payload: credentials })
+
+}
+
+export const loginFail = (err) => dispatch => {
+    dispatch({ type: LOGIN_FAIL, payload: err })
 }
 
 export const logoutAC = () => dispatch => {
--- use-my-tools/src/store/reducers/loginReducer.js
+++ use-my-tools/src/store/reducers/loginReducer.js
@@ -1,5 +1,7 @@
 import {
+    LOGIN_START,
     LOGIN_SUCCESS,
+    LOGIN_FAIL,
     LOGOUT_SUCCESS
 } from '../actions'
 
@@ -8,16 +10,31 @@ const initalState = {
     loggedIn: Boolean(localStorage.getItem('token')),
     authToken: null,
     userid: "",
+    loggingStatus: false,
 }
 
 export default function loginReducer(state = initalState, action) {
     switch(action.type){
+        case LOGIN_START: 
+            return {
+                ...state,
+                loggingStatus: true
+            }
+
         case LOGIN_SUCCESS: 
             return {
                 ...state,
+                loggedIn: true,
+                loggingStatus: false,
                 authToken: action.payload,
-                loggedIn: true
             }
+
+        case LOGIN_FAIL: 
+        return {
+            ...state,
+            loggingStatus: false
+        }
+
 
         case LOGOUT_SUCCESS: 
             return {
