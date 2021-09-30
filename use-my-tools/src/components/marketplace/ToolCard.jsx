bc7051128470f51553b56cf7166ec04b8b2eac57
Matthew Meeves <matthew.meeves@gmail.com>
2021-09-30 18:18:33 -0500

    ran prettier on toolcard

--- use-my-tools/src/components/marketplace/ToolCard.jsx
+++ use-my-tools/src/components/marketplace/ToolCard.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 
 //componenets
-import RequestTool from "./RequestTool"
+import RequestTool from "./RequestTool";
 
 // styles
-import * as styled from "../styled-components/toolCard"
+import * as styled from "../styled-components/toolCard";
 import missingPhoto from "../../assets/missing-image.png";
 
 function ToolCard(props) {
   return (
     <styled.Card>
-        <styled.PriceTag>
-          <p>${props.tool.rental_cost}</p>
-        </styled.PriceTag>
+      <styled.PriceTag>
+        <p>${props.tool.rental_cost}</p>
+      </styled.PriceTag>
       <styled.ToolContent>
-        
-        <styled.ToolTitle> 
-          {props.tool.tool_name}
-        </styled.ToolTitle>
-        {!props.tool.img_url
-        ?
-        <img src={missingPhoto} alt='missing'/>
-        :
-        <img src={props.tool.img_url} alt={props.tool.tool_name}/>
-      }
+        <styled.ToolTitle>{props.tool.tool_name}</styled.ToolTitle>
+        {!props.tool.img_url ? (
+          <img src={missingPhoto} alt="missing" />
+        ) : (
+          <img src={props.tool.img_url} alt={props.tool.tool_name} />
+        )}
         <h4>Owner: {props.tool.user_name}</h4>
 
         <h4>Type: {props.tool.tool_type}</h4>
         <h4>Tool Description: {props.tool.tool_description}</h4>
 
-        <RequestTool toolID={props.tool.id} usersTool={props.tool.user_name === localStorage.getItem('username')}/>
+        <RequestTool
+          toolID={props.tool.id}
+          usersTool={props.tool.user_name === localStorage.getItem("username")}
+        />
       </styled.ToolContent>
     </styled.Card>
   );
